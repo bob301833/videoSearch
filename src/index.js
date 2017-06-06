4823f20b1419892bb640bc54fb71635eef8be120
yuhsing <bob301833@gmail.com>
2017-06-06 14:19:03 +0800

    refactor Funciotn components to Class Components

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAKPIDmexDmPWEX3fLYiUQa3gAc8jwZwZE';
 
+
+
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's  generated HTML and put it
